201ec97e67e1b763268cb6b6bb70d8ae4634c673
Xiao Ming <bluecomet2004@outlook.com>
2023-06-01 15:54:45 -0400

    completed server side with API integration

--- server/app/models.py
+++ server/app/models.py
@@ -1,5 +1,9 @@
 import uuid
+import jwt
+import datetime
 from django.db import models
+from typing import Dict, Any
+
 
 class UserModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -14,7 +18,7 @@ class UserModel(models.Model):
         ordering = ['createdAt']
         
         def __str__(self) -> str:
-            return self.name
+            return self.email
         
 
 class GameHistoryModel(models.Model):
@@ -22,6 +26,8 @@ class GameHistoryModel(models.Model):
     name = models.CharField(max_length=128)
     email = models.EmailField()
     score = models.FloatField()
+    difficulty = models.CharField(max_length=128)
+    matrixSize = models.IntegerField()
     limitTime = models.IntegerField()
     overTime = models.IntegerField()
     finishedAt = models.DateTimeField(auto_now_add=True)
@@ -32,3 +38,23 @@ class GameHistoryModel(models.Model):
 
         def __str__(self) -> str:
             return self.finishedAt
+        
+
+def generate_token(user: UserModel, secret_key: str, duration: int) -> str:
+    payload: Dict[str, Any] = {
+        "id": str(user.id),
+        "name": user.name,
+        "email": user.email,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=duration)
+    }
+
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
+    return token
+
+def get_user_from_token(token: str, secret_key: str):
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=["HS256"])
+        user_id = payload.get("id")
+        return UserModel.objects.get(id=user_id)
+    except (jwt.InvalidTokenError, UserModel.DoesNotExist):
+        return None
--- server/app/urls.py
+++ server/app/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path, include
-from rest_framework import routers
-from app.views import UserView, LoginView, GameHistoryView
+from django.urls import path
+from app.views import RegisterView, LoginView, TokenView, UserView, GameHistoryView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('users/', UserView.as_view()),
     path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('token/', TokenView.as_view()),
+    path('users/', UserView.as_view()),
     path('history/', GameHistoryView.as_view())
 ]
