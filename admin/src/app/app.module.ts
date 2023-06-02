a3897426a06cbd7970ca1cdb6c3f8fe7f6d9859d
Xiao Ming <bluecomet2004@outlook.com>
2023-06-02 07:16:17 -0400

    created admin using Angular

---
+++ admin/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './dashboard/user/user.component';
+import { HistoryComponent } from './dashboard/history/history.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    HistoryComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
