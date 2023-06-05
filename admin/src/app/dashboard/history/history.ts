fc2406cdb747bf2c9705714da6bdeaaa2134b7ed
Xiao Ming <bluecomet2004@outlook.com>
2023-06-05 08:52:32 -0400

    started history component

---
+++ admin/src/app/dashboard/history/history.ts
@@ -0,0 +1,20 @@
+export interface History {
+  id: string;
+  name: string;
+  email: string;
+  score: number;
+  difficulty: string;
+  matrixSize: number;
+  limitTime: number;
+  overTime: number;
+  finishedAt: Date;
+}
+
+export interface HistoryDashboard {
+  success: boolean;
+  total: number;
+  current_page: number;
+  last_page: number;
+  list: History[];
+}
+
